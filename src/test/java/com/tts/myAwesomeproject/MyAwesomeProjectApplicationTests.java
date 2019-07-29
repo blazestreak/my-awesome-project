5950bc9cc14ea137d051b758afc6ba439232d93a
Luke Johnson <blazestreak@Lukes-MacBook-Pro.local>
2019-07-29 12:38:50 -0400

    first commit

---
+++ src/test/java/com/tts/myAwesomeproject/MyAwesomeProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tts.myAwesomeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyAwesomeProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
